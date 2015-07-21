47f8f332d76a82abde65d7e7b3e78e56814b4921
archersa <archersa@Archers-MacBook-Air.local>
2015-07-20 19:50:27 -0700

    End of Bolt Lesson

---
+++ Assets/Scrips/Mover.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Mover : MonoBehaviour
+
+{
+	public float speed;
+
+	Rigidbody rigidbody;
+
+	void Start () 
+	{
+		rigidbody = GetComponent<Rigidbody> ();
+	
+  	    rigidbody.velocity = transform.forward * speed;
+	}
+
+}
