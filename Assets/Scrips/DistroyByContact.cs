b5928b1ae1df8f395c4bc18ec35843cf956c8c24
archersa <archersa@Archers-MacBook-Air.local>
2015-07-25 10:13:03 -0700

    Moving Randomized Asteroid

--- Assets/Scrips/DistroyByContact.cs
+++ Assets/Scrips/DistroyByContact.cs
@@ -3,11 +3,19 @@ using System.Collections;
 
 public class DistroyByContact : MonoBehaviour 
 {
+	public GameObject explosion;
+	public GameObject PlayerExplosion;
+
+
 	void OnTriggerEnter(Collider other) {
 		if (other.tag == "Boundary")
 		{
 			return;
 		}
+		Instantiate(explosion, transform.position, transform.rotation);		
+		if (other.tag == "Player") {	
+			Instantiate (PlayerExplosion, other.transform.position, other.transform.rotation);	
+		}	
 		Destroy (other.gameObject);
 		Destroy (gameObject);
 	}
