72d33d9afcfb45c441318829e181a5b5989c1b47
archersa <archersa@Archers-MacBook-Air.local>
2015-08-20 20:16:15 -0700

    Workable on phone

--- Assets/Scrips/GameController.cs
+++ Assets/Scrips/GameController.cs
@@ -11,15 +11,34 @@ public class GameController : MonoBehaviour
 	public float waveWait;
 
 	public GUIText scoreText;
+	public GUIText restartText;
+	public GUIText gameOverText;
+
+	private bool gameOver;
+	private bool restart;
 	private int score;
 	
 	void Start ()
 	{
+		gameOver = false;
+		restart = false;
+		restartText.text = "";
+		gameOverText.text = ""; 
 		score = 0;
 		UpdateScore ();
 		StartCoroutine (SpawnWaves ());
 	}
-	
+	void Update ()
+	{
+		if (restart) 
+		{
+			if (Input.GetKeyDown (KeyCode.R))
+			{
+				Application.LoadLevel (Application.loadedLevel);
+			}
+		}
+	}
+
 	IEnumerator SpawnWaves ()
 	{
 		yield return new WaitForSeconds (startWait);
@@ -33,6 +52,13 @@ public class GameController : MonoBehaviour
 				yield return new WaitForSeconds (spawnWait);
 			}
 			yield return new WaitForSeconds (waveWait);
+		
+			if (gameOver)
+			{
+				restartText.text = "Press 'R' for Restart";
+				restart = true;
+				break;
+			}
 		}
 	}
 	public void AddScore (int newScoreVaule)
@@ -45,5 +71,10 @@ public class GameController : MonoBehaviour
 	void UpdateScore ()
 	{
 		scoreText.text = "Score: " + score;
+	}
+	public void GameOver ()
+	{
+		gameOverText.text = "Game Over!";
+		gameOver = true;
 	}
 }
